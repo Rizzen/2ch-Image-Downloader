62017fdd516e9e1a42ff2fd4fdf92563b766af00
Marcus Weaver <onemangd@gmail.com>
2017-02-10 21:53:21 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -2,14 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
+using System.Net.Mime;
 using HtmlAgilityPack;
 
-namespace _2chparse
+namespace DvachImages
 {
     class Program
     {
         public static List<string> imgLinks;
         public static WebClient _webClient;
+        public static WebRequest _webRequest;
 
         public static void GetImages(HtmlDocument htmlDoc)
         {
@@ -20,7 +22,10 @@ namespace _2chparse
                 string link = node.SelectSingleNode("div/a").Attributes["href"].Value;
                 Console.WriteLine("--------" + link);
                 Console.WriteLine("Extention ---- " + link.Substring(link.Length - 4));
-                _webClient.DownloadFile("https://2ch.pm" + link, "Image " + i + link.Substring(link.Length - 4));
+                _webClient.DownloadFile("https://2ch.hk" + link, "Image " + i + link.Substring(link.Length - 4));
+                ContentType contentType = new ContentType(_webClient.ResponseHeaders.GetValues("Content-Type")[0]);
+
+                Console.WriteLine("\n------- " + contentType.MediaType.Split(new char[] {'/'}).Last() + " --------\n");
                 i++;
             }
         }
@@ -30,14 +35,16 @@ namespace _2chparse
         static void Main()
         {
             imgLinks = new List<string>();
-            string remoteURI = "https://2ch.pm/pr/res/890393.html";
+            string remoteURI = "https://2ch.hk/pr/res/927547.html";
             HtmlDocument doc = new HtmlDocument();
             _webClient = new WebClient();
+
             _webClient.Encoding = System.Text.Encoding.GetEncoding("utf-8");
 
             Console.WriteLine("...............Request.............\n");
 
             doc.LoadHtml(_webClient.DownloadString(remoteURI));
+
             GetImages(doc);
 
             Console.WriteLine("----------------Done---------------");
