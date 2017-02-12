ab2c7e40a9c3a1ac915fa742dd3c1ef6798d3db3
Marcus Weaver <onemangd@gmail.com>
2017-02-13 01:09:40 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
-using System.Net.Mime;
 using HtmlAgilityPack;
 
 namespace DvachImages
@@ -11,21 +10,25 @@ namespace DvachImages
     {
         public static List<string> imgLinks;
         public static WebClient _webClient;
-        public static WebRequest _webRequest;
 
         public static void GetImages(HtmlDocument htmlDoc)
         {
             Console.WriteLine(htmlDoc.DocumentNode.SelectNodes("//*[@class='image']").Count());
             int i = 0;
+            
             foreach (HtmlNode node in htmlDoc.DocumentNode.SelectNodes("//*[@class='image']"))
             {
+                string format;
                 string link = node.SelectSingleNode("div/a").Attributes["href"].Value;
                 Console.WriteLine("--------" + link);
-                Console.WriteLine("Extention ---- " + link.Substring(link.Length - 4));
-                _webClient.DownloadFile("https://2ch.hk" + link, "Image " + i + link.Substring(link.Length - 4));
-                ContentType contentType = new ContentType(_webClient.ResponseHeaders.GetValues("Content-Type")[0]);
 
-                Console.WriteLine("\n------- " + contentType.MediaType.Split(new char[] {'/'}).Last() + " --------\n");
+                using (WebResponse response = WebRequest.Create(String.Format("https://2ch.hk{0}", link)).GetResponse())
+                {
+                    format = response.Headers.GetValues("Content-Type")[0].Split(new char[] { '/' }).Last();
+                    Console.WriteLine("\n------- " + format + " --------\n");
+                }
+
+                _webClient.DownloadFile("https://2ch.hk" + link, "Image " + i + "." + format);
                 i++;
             }
         }
@@ -35,19 +38,16 @@ namespace DvachImages
         static void Main()
         {
             imgLinks = new List<string>();
-            string remoteURI = "https://2ch.hk/pr/res/927547.html";
+            string remoteURI = "https://2ch.hk/v/res/1765234.html";
             HtmlDocument doc = new HtmlDocument();
             _webClient = new WebClient();
-
             _webClient.Encoding = System.Text.Encoding.GetEncoding("utf-8");
 
             Console.WriteLine("...............Request.............\n");
-
             doc.LoadHtml(_webClient.DownloadString(remoteURI));
-
             GetImages(doc);
-
             Console.WriteLine("----------------Done---------------");
+
             Console.ReadLine();
         }
     }
